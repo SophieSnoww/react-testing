2eb438275052b9cc6be35af07cb37a252aa9c220
Sylas <38060892+glacierheavner@users.noreply.github.com>
2022-02-06 22:21:12 -0700

    Fixed some data stuff

--- src/App.js
+++ src/App.js
@@ -6,17 +6,37 @@ import Visualizer from './Visualizer';
 class App extends React.Component {
     constructor(props) {
         super(props);
-
-        this.ref = React.createRef();
+        
+        let m = 1;
+        let luminosity = this.truncate(m ** 4);
         
         this.state = {
-            starMass: 1
+            mass: m,
+            luminosity: this.truncate(m ** 4),
+            radius: this.truncate(m ** 0.74),
+            lifespan: this.truncate(m ** -2.5),
+            temp: this.truncate(m ** 0.505),
+            iBoundry: this.truncate(m * 0.1),
+            hZone: this.truncate(luminosity ** 0.5),
+            fLine: this.truncate((luminosity ** 0.5) * 4.85),
+            oBoundry: this.truncate(m * 40)
         }
     }
     
     handleInput(data) {
+        let m = Number(data.target.value);
+        let luminosity = this.truncate(m ** 4);
+        
         this.setState({
-            starMass: Number(data.target.value)
+            mass: Number(data.target.value),
+            luminosity: this.truncate(m ** 4),
+            radius: this.truncate(m ** 0.74),
+            lifespan: this.truncate(m ** -2.5),
+            temp: this.truncate(m ** 0.505),
+            iBoundry: this.truncate(m * 0.1),
+            hZone: this.truncate(luminosity ** 0.5),
+            fLine: this.truncate((luminosity ** 0.5) * 4.85),
+            oBoundry: this.truncate(m * 40)
         });
     }
     
@@ -26,29 +46,38 @@ class App extends React.Component {
     }
     
     render() {
-        let m = this.state.starMass;
-
-        let luminosity = this.truncate(m ** 4);
-        let radius = this.truncate(m ** 0.74);
-        let lifespan = this.truncate(m ** -2.5);
-        let temp = this.truncate(m ** 0.505);
-        let iBoundry = this.truncate(m * 0.1);
-        let hZone = this.truncate(luminosity ** 0.5)
-        let fLine = this.truncate((luminosity ** 0.5) * 4.85)
-        let oBoundry = this.truncate(m * 40);
+        let startingOrbit = this.state.fLine + 1;
 
         return (
             <div className="App">
-                <Visualizer data={{mass: m, luminosity, radius, lifespan, temp, iBoundry, hZone, fLine, oBoundry, ref: this.ref}} />
+                <Visualizer data={this.state} />
                 <Input handleInput={(data) => this.handleInput(data)} />
-                <div> Luminosity: {luminosity} </div>
-                <div> Radius: {radius} </div>
-                <div> Surface Tempreture: {temp} ({Math.round(temp*5778)} K) </div>
-                <div> Lifespan: {lifespan * 10} billion years </div>
-                <div> Inner Boundry: {iBoundry} AU </div>
-                <div> Habitable Zone: {this.truncate(hZone * 0.95)} AU - {this.truncate(hZone * 1.37)} AU </div>
-                <div> Frost Line: {fLine} AU </div>
-                <div> Outer Boundry: {oBoundry} AU </div>
+                <div className="data-container">
+                    <div className="data-output">
+                        <div className="data-output-title">Luminosity:</div> {this.state.luminosity}
+                    </div>
+                    <div className="data-output">
+                        <div className="data-output-title">Radius:</div> {this.state.radius}
+                    </div>
+                    <div className="data-output">
+                        <div className="data-output-title">Surface Tempreture:</div> {this.state.temp} ({Math.round(this.state.temp*5778)} K)
+                    </div>
+                    <div className="data-output">
+                        <div className="data-output-title">Lifespan:</div> {this.truncate(this.state.lifespan * 10)} billion years
+                    </div>
+                    <div className="data-output">
+                        <div className="data-output-title">Inner Boundry:</div> {this.state.iBoundry} AU
+                    </div>
+                    <div className="data-output">
+                        <div className="data-output-title">Habitable Zone:</div> {this.truncate(this.state.hZone * 0.95)} AU - {this.truncate(this.state.hZone * 1.37)} AU
+                    </div>
+                    <div className="data-output">
+                        <div className="data-output-title">Frost Line:</div> {this.state.fLine} AU
+                    </div>
+                    <div className="data-output">
+                        <div className="data-output-title">Outer Boundry:</div> {this.state.oBoundry} AU
+                    </div>
+                </div>
             </div>
         );
     }
