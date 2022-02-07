f5532a5bf8d0263186c0ec181d3555f329219bd2
Sylas <38060892+glacierheavner@users.noreply.github.com>
2022-02-06 21:36:11 -0700

    YAY GOOD SCALING

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -8,21 +8,21 @@ class Visualizer extends React.Component {
     }
 
     render() {
-        let appWidth = window.innerWidth -40;
-        let arcStart = 0;
-        let arcEnd = appWidth - 125;
+        let appWidth = window.innerWidth - 40;
+        let arcEnd = appWidth - 50;
 
-        let iBoundry = arcEnd * (this.props.data.iBoundry / this.props.data.oBoundry);
-        let fLine = arcEnd * (this.props.data.fLine / this.props.data.oBoundry);
-        let hZone = arcEnd * (this.props.data.hZone / this.props.data.oBoundry);
+        let iBoundry = arcEnd * Math.sqrt(this.props.data.iBoundry / this.props.data.oBoundry);
+        let hZone = arcEnd * Math.sqrt(this.props.data.hZone / this.props.data.oBoundry);
+        let fLine = arcEnd * Math.sqrt(this.props.data.fLine / this.props.data.oBoundry);
+        let oBoundry = arcEnd * Math.sqrt(1);
 
         return (
             <div className="Visualizer">
-                {/* <star /> */}
+                <star />
                 <Arc color="#f00" width={iBoundry * 2} />
                 <Arc color="#0f0" width={hZone * 2} />
                 <Arc color="#0ff" width={fLine * 2} />
-                <Arc color="#f00" width={arcEnd * 2} />
+                <Arc color="#f00" width={oBoundry * 2} />
             </div>
         );
     }
