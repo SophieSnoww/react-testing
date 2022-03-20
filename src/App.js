de9780a3e2bcd7bcdccd788e8a20dd541545b9dc
Sophie Snow <sophie.snoww@pm.me>
2022-03-20 12:09:53 -0600

    Not sure

--- src/App.js
+++ src/App.js
@@ -9,34 +9,34 @@ import Visualizer from './Visualizer';
 class App extends React.Component {
     constructor(props) {
         super(props);
-        
+
         let m = 1;
         let luminosity = this.truncate(m ** 4);
         let iBoundry = this.truncate(m * 0.1);
         let fLine = this.truncate((luminosity ** 0.5) * 4.85);
         let oBoundry = this.truncate(m * 40);
-        
+
         let startingOrbit = fLine + 1;
         let orbit = startingOrbit;
         let orbits = [startingOrbit];
-        
+
         while (orbit < oBoundry) {
             orbit *= 1.4 + (Math.floor(Math.random() * 60) / 100);
             orbits.push(orbit);
         }
-        
-        orbits.pop(orbits.length-1);
-        
+
+        orbits.pop(orbits.length - 1);
+
         orbit = startingOrbit;
-        
+
         while (orbit > iBoundry) {
             orbit /= 1.4 + (Math.floor(Math.random() * 60) / 100);
             orbits.splice(0, 0, orbit);
         }
-        
+
         orbits.splice(0, 1);
         orbits.splice(0, 1);
-        
+
         this.state = {
             mass: m,
             luminosity: this.truncate(m ** 4),
@@ -51,7 +51,7 @@ class App extends React.Component {
             orbitsDisabled: false
         }
     }
-    
+
     handleInput(data) {
         let m = Number(data.target.value);
         if ([0, NaN].includes(m) || m < 0.4 || m > 10) {
@@ -65,28 +65,28 @@ class App extends React.Component {
         let hZone = this.truncate(luminosity ** 0.5);
         let fLine = this.truncate((luminosity ** 0.5) * 4.85);
         let oBoundry = this.truncate(m * 40);
-        
+
         let startingOrbit = fLine + 1;
         let orbit = startingOrbit;
         let orbits = [startingOrbit];
-        
+
         while (orbit < this.state.oBoundry) {
             orbit *= 1.4 + (Math.floor(Math.random() * 60) / 100);
             orbits.push(orbit);
         }
-        
-        orbits.pop(orbits.length-1);
-        
+
+        orbits.pop(orbits.length - 1);
+
         orbit = startingOrbit;
-        
+
         while (orbit > this.state.iBoundry) {
             orbit /= 1.4 + (Math.floor(Math.random() * 60) / 100);
             orbits.splice(0, 0, orbit);
         }
-        
+
         orbits.splice(0, 1);
         orbits.splice(0, 1);
-        
+
         this.setState({
             mass: m,
             luminosity,
@@ -100,82 +100,85 @@ class App extends React.Component {
             orbits
         });
     }
-    
+
     regenOrbits() {
         let startingOrbit = this.state.fLine + 1;
         let orbit = startingOrbit;
         let orbits = [startingOrbit];
-        
+
         while (orbit < this.state.oBoundry) {
             orbit *= 1.4 + (Math.floor(Math.random() * 60) / 100);
             orbits.push(orbit);
         }
-        
+
         orbits.pop();
-        
+
         orbit = startingOrbit;
-        
+
         while (orbit > this.state.iBoundry) {
             orbit /= 1.4 + (Math.floor(Math.random() * 60) / 100);
             orbits.splice(0, 0, orbit);
         }
-        
+
         orbits.splice(0, 1);
         orbits.splice(0, 1);
-        
+
         this.setState({
             orbits
         });
     }
-    
+
     toggleOrbits() {
         this.setState({
             orbitsDisabled: !this.state.orbitsDisabled
         });
     }
-    
-    truncate(number, places=3) {
+
+    truncate(number, places = 3) {
         let num = 10 ** places;
-        return Math.round(number*num)/num;
+        return Math.round(number * num) / num;
     }
-    
+
     render() {
         return (
             <div className="App">
-                <OrbitDisplay data={this.state} />
-                <OrbitToggle data={this.state} handleClick={() => this.toggleOrbits()} />
-                <RegenButton handleClick={() => this.regenOrbits()} />
                 <Visualizer data={this.state} />
-                <Input handleInput={(data) => this.handleInput(data)} />
-                <div className="data-container">
-                    <div className="data-output">
-                        <div className="data-output-title">Luminosity:</div> {this.state.luminosity}
+                <div className="app-container">
+
+                    <OrbitDisplay data={this.state} />
+                    <OrbitToggle data={this.state} handleClick={() => this.toggleOrbits()} />
+                    <RegenButton handleClick={() => this.regenOrbits()} />
+                    <Input handleInput={(data) => this.handleInput(data)} />
+                    <div className="data-container">
+                        <div className="data-output">
+                            <div className="data-output-title">Luminosity:</div> {this.state.luminosity}
+                        </div>
+                        <div className="data-output">
+                            <div className="data-output-title">Radius:</div> {this.state.radius}
+                        </div>
+                        <div className="data-output">
+                            <div className="data-output-title">Surface Tempreture:</div> {this.state.temp} ({Math.round(this.state.temp * 5778)} K)
                     </div>
-                    <div className="data-output">
-                        <div className="data-output-title">Radius:</div> {this.state.radius}
+                        <div className="data-output">
+                            <div className="data-output-title">Lifespan:</div> {this.truncate(this.state.lifespan * 10)} billion years
                     </div>
-                    <div className="data-output">
-                        <div className="data-output-title">Surface Tempreture:</div> {this.state.temp} ({Math.round(this.state.temp*5778)} K)
+                        <div className="data-output">
+                            <div className="data-output-title">Inner Boundry:</div> {this.state.iBoundry} AU
                     </div>
-                    <div className="data-output">
-                        <div className="data-output-title">Lifespan:</div> {this.truncate(this.state.lifespan * 10)} billion years
+                        <div className="data-output">
+                            <div className="data-output-title">Habitable Zone:</div> {this.truncate(this.state.hZone * 0.95)} AU - {this.truncate(this.state.hZone * 1.37)} AU
                     </div>
-                    <div className="data-output">
-                        <div className="data-output-title">Inner Boundry:</div> {this.state.iBoundry} AU
+                        <div className="data-output">
+                            <div className="data-output-title">Frost Line:</div> {this.state.fLine} AU
                     </div>
-                    <div className="data-output">
-                        <div className="data-output-title">Habitable Zone:</div> {this.truncate(this.state.hZone * 0.95)} AU - {this.truncate(this.state.hZone * 1.37)} AU
+                        <div className="data-output">
+                            <div className="data-output-title">Outer Boundry:</div> {this.state.oBoundry} AU
                     </div>
-                    <div className="data-output">
-                        <div className="data-output-title">Frost Line:</div> {this.state.fLine} AU
                     </div>
-                    <div className="data-output">
-                        <div className="data-output-title">Outer Boundry:</div> {this.state.oBoundry} AU
+                    <div className="credits">
+                        <a href="https://github.com/HouseholdDragon" className="credit-link">HouseholdDragon</a>
+                        <a href="https://sophie-snow.carrd.co/" className="credit-link">Sophie Snoww</a>
                     </div>
-                </div>
-                <div className="credits">
-                    <a href="https://github.com/HouseholdDragon" className="credit-link">HouseholdDragon</a>
-                    <a href="https://sophie-snow.carrd.co/" className="credit-link">Sophie Snoww</a>
                 </div>
             </div>
         );
--- src/Arc.js
+++ src/Arc.js
@@ -10,9 +10,10 @@ class Arc extends React.Component {
         return (
             <div className="arc" style={{
                 border: "1px solid " + this.props.color,
-                width: this.props.width + "px",
-                height: this.props.width + "px",
-                left: -(this.props.width / 2) + "px",
+                // width: this.props.width + "px",
+                // height: this.props.width + "px",
+                // left: -(this.props.width / 2) + "px",
+                "--width": this.props.width + "px",
                 opacity: (this.props.isDisabled) ? 0 : ((this.props.isOrbit) ? 0.5 : 1)
             }}>
                 <div className="arc-label" style={{color: this.props.color}}>{this.props.label}</div>
